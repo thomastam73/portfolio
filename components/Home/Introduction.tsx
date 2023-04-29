2e12a76daf523ae722d41d25618bc1876245f902
thomastam73 <thomastam73@gmail.com>
2023-04-29 22:59:21 +0800

    fix: deployment

--- components/Home/Introduction.tsx
+++ components/Home/Introduction.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { FC } from "react";
 
 const Introduction = () => {
   return (
@@ -8,7 +7,7 @@ const Introduction = () => {
         <div className="grid grid-flow-row gap-y-8">
           <Title />
           <p className="text-xl text-zinc-100">
-            Hi, I'm Thomas, A Front-End and Back-End lover.
+            {"Hi, I'm Thomas, A Front-End and Back-End lover."}
           </p>
         </div>
       </div>
@@ -20,7 +19,7 @@ const Introduction = () => {
 const Title = () => {
   return (
     <p
-      className="animate-text bg-gradient-to-r from-orange-500 via-orange-300 to-orange-500 
+      className="animate-text bg-gradient-to-r from-orange-500 via-yellow-500 to-red-400
     bg-clip-text text-5xl font-bold text-transparent 
     drop-shadow-[6px_6px_4px_rgba(253,186,116,0.5)]"
     >
